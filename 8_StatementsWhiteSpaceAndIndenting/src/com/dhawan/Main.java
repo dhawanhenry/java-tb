4315e907fcf595c7e2a1efd19ddf0b02a7a86a7b
dhawanhenry <dhawan.henry@gmail.com>
2019-04-04 01:07:49 +0200

    Quick change to verify git push without credentials

--- 8_StatementsWhiteSpaceAndIndenting/src/com/dhawan/Main.java
+++ 8_StatementsWhiteSpaceAndIndenting/src/com/dhawan/Main.java
@@ -7,5 +7,7 @@ public class Main {
         myVariable++;
         myVariable--;
         System.out.println("This is a test, " + myVariable);
+        int newVariable = myVariable + 10;
+        System.out.println("Test for GIT push, result is " + newVariable);
     }
 }
