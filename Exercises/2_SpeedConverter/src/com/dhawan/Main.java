9388749e86c2c3e2fd315f7ff2a53b3bd39d02bc
dhawanhenry <dhawan.henry@gmail.com>
2019-04-06 20:26:44 +0200

    added Exercise 2_SpeedConverter

---
+++ Exercises/2_SpeedConverter/src/com/dhawan/Main.java
@@ -0,0 +1,29 @@
+package com.dhawan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        printConversion(60.5);
+        printConversion(96);
+        printConversion(124.66);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            long convertMilesPerHour = Math.round(kilometersPerHour / 1.609);
+            return convertMilesPerHour;
+        }
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            System.out.println("Invalid value");
+        } else {
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+        }
+    }
+}
