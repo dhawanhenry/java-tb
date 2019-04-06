9e86ff55ecc9a07484a9fc8e560d152e2508ca1b
dhawanhenry <dhawan.henry@gmail.com>
2019-04-06 18:15:56 +0200

    added exercises folder to repo

---
+++ Exercises/1_CheckNumerProject/src/com/dhawan/Main.java
@@ -0,0 +1,21 @@
+package com.dhawan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        checkNumber(11);
+        checkNumber(0);
+        checkNumber(-343);
+    }
+
+    public static void checkNumber(int number){
+        if(number > 0){
+            System.out.println("positive");
+        } else if(number < 0){
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
+    }
+}
